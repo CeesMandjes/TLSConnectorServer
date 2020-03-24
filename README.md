f9b793602d6a019f0808a1c934c376bf612b94b2
Cees Mandjes <ceesmandjes@gmail.com>
2020-03-24 12:38:49 +0100

    Added comments and README for configuration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TLSConnectorServer
-Server implementation for the TLSConnector Android application. This server performs Android SafetyNet check for its client.
+Server implementation for Android SafetyNet Attestation check. This implementation provides a nonce and validates SafetyNet's signed attestation. The SafetyNet check will pass when the integrity of the Android device and application is satisfied.
+
+Path to request nonce (GET request): '/index.php/api/getnonce'. <br/>
+Path to validates SafetyNet's signed attestation (POST request): '/index.php/api/validatejws'. <br/>
+    The signed attestation needs to in a POST request encapsulated in variable with the name 'jws'.
+
+Configuration:
+- Set package name of Android application in '$APKpackageNameExpected'.
+- Set certificate hash which signed the APK of the Android application in '$APKCertificateDigestSha256Expected'.
--- src/index.php
+++ src/index.php
@@ -1,4 +1,19 @@
 <?php
+/*
+    Server implementation for Android SafetyNet Attestation check. This implementation provides a nonce and validates SafetyNet's signed attestation. 
+    The SafetyNet check will pass when the integrity of the Android device and application is satisfied.
+
+    Path to request nonce (GET request): '/index.php/api/getnonce'
+    Path to validates SafetyNet's signed attestation (POST request): '/index.php/api/validatejws' 
+        The signed attestation needs to in a POST request encapsulated in variable with the name 'jws'
+
+    Configuration:
+    - Set package name of Android application in '$APKpackageNameExpected'
+    - Set certificate hash which signed the APK of the Android application in '$APKCertificateDigestSha256Expected'
+
+    @author Cees Mandjes
+*/
+
 // Database file for the Nonce for SafetyNet session
 $nonceFile = "nonce.txt";
 // Path to the CA certificate of SafetyNet's attestation
